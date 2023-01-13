28acda275245a058018f87a88ad940bf2fbef2fb
aqifsariman <aqifsariman@gmail.com>
2023-01-13 22:47:54 +0800

    Removed more comments.

--- src/gameOfLife/Main.java
+++ src/gameOfLife/Main.java
@@ -6,9 +6,5 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Game game = new Game(args[0]);
         game.gameStart();
-        // int[][] boardCreation = game.gameStart();
-        // int[][] originalBoard = game.createBoard(game.getWidth(), game.getHeight(),
-        // boardCreation);
-
     }
 }
